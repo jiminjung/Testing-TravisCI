646b22000ec84f638c255f54add4608c717bc479
jiminjung <jimin606@naver.com>
2022-05-31 11:41:46 +0900

    jdk version changed

--- src/main/java/CalMain.java
+++ src/main/java/CalMain.java
@@ -1,16 +1,22 @@
 
 public class CalMain {
-	CalIF cal;
-	public void setCal(CalIF cal) {
-		this.cal = cal;
-	}
-  // return sum of a and b
-	public int doAdd(int a,int b){
-		System.out.println("## summing "+a+"+"+b+"="+ cal.add(a, b) );
-		return cal.add(a, b);
-	}
-	public int doSub(int a,int b){
-		System.out.println("## substracting "+a+"+"+b+"="+ cal.substract(a, b) );
-		return cal.substract(a, b);
-	} 
+
+		
+		CalIF cal;
+		
+		public void setCal(CalIF cal) {
+			this.cal = cal;
+		}
+		
+	  // return sum of a and b
+		public int doAdd(int a,int b){
+			System.out.println("## summing "+a+"+"+b+"="+ cal.add(a, b) );
+			return cal.add(a, b);
+		}
+		
+		public int doSub(int a,int b){
+			System.out.println("## substracting "+a+"+"+b+"="+ cal.substract(a, b) );
+			return cal.substract(a, b);
+		}
+		
 }
