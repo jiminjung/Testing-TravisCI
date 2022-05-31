14fdceb1450d408ece4e2e9abb80d99c664b813a
jiminjung <jimin606@naver.com>
2022-05-31 12:52:31 +0900

    lalala

--- src/main/java/CalTest.java
+++ src/main/java/CalTest.java
@@ -1,7 +1,5 @@
-import static org.easymock.EasyMock.aryEq;
 import static org.easymock.EasyMock.createMock;
 import static org.easymock.EasyMock.expect;
-import static org.easymock.EasyMock.expectLastCall;
 import static org.easymock.EasyMock.replay;
 import static org.easymock.EasyMock.verify;
 
